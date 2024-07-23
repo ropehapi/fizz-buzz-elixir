455c3747abab3316cd8d271ccd2d1bf4e534743c
ropehapi <ropehapi@gmail.com>
2024-07-23 12:14:26 -0300

    Convertendo nossa string em uma lista de ints

--- lib/fizz_buzz_elixir.ex
+++ lib/fizz_buzz_elixir.ex
@@ -6,6 +6,14 @@ defmodule FizzBuzzElixir do
     |> handle_file_read
   end
 
+  def handle_file_read({:ok, result}) do
+    result
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def handle_file_read({:error, reason}), do: "Error reading the file: #{reason}"
+
   def build_alternative_1(file_name) do
     file = File.read(file_name)
     handle_file_read(file)
@@ -18,6 +26,13 @@ defmodule FizzBuzzElixir do
     end
   end
 
-  def handle_file_read({:ok, result}), do: result
-  def handle_file_read({:error, reason}), do: reason
+  def handle_file_read_alternative_1({:ok, result}) do
+    list = String.split(result, ",")
+    Enum.map(list, fn number -> String.to_integer(number) end)
+  end
+
+  def handle_file_read_alternative_2({:ok, result}) do
+    list = String.split(result, ",")
+    Enum.map(list, &String.to_integer/1)
+  end
 end
