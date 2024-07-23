6296e64e74512a30dd82585aec96084d233b09c0
ropehapi <ropehapi@gmail.com>
2024-07-23 12:06:21 -0300

    Lendo o arquivo usando pattern matching

--- lib/fizz_buzz_elixir.ex
+++ lib/fizz_buzz_elixir.ex
@@ -1,18 +1,23 @@
 defmodule FizzBuzzElixir do
-  @moduledoc """
-  Documentation for `FizzBuzzElixir`.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> FizzBuzzElixir.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def build(file_name) do
+    file_name
+    |> File.read
+    |> handle_file_read
   end
+
+  def build_alternative_1(file_name) do
+    file = File.read(file_name)
+    handle_file_read(file)
+  end
+
+  def build_alternative_2(file_name) do
+    case File.read(file_name) do
+      {:ok, result} -> result
+      {:error, reason} -> reason
+    end
+  end
+
+  def handle_file_read({:ok, result}), do: result
+  def handle_file_read({:error, reason}), do: reason
 end
