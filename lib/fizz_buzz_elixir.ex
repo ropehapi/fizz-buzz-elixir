c67d64929f8dbdafabf2039eacfa50fd1639221a
ropehapi <ropehapi@gmail.com>
2024-07-24 01:02:36 -0300

    Algoritmo fizzbuzz sendo aplicado corretamente

--- lib/fizz_buzz_elixir.ex
+++ lib/fizz_buzz_elixir.ex
@@ -9,10 +9,21 @@ defmodule FizzBuzzElixir do
   def handle_file_read({:ok, result}) do
     result
     |> String.split(",")
-    |> Enum.map(&String.to_integer/1)
+    |> Enum.map(&convert_and_evaluate_numbers/1)
   end
 
   def handle_file_read({:error, reason}), do: "Error reading the file: #{reason}"
+
+  def convert_and_evaluate_numbers(elem) do
+    number = String.to_integer(elem)
+    evaluate_numbers(number)
+
+  end
+
+  def evaluate_numbers(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
+  def evaluate_numbers(number) when rem(number, 5) == 0, do: :buzz
+  def evaluate_numbers(number) when rem(number, 3) == 0, do: :fizz
+  def evaluate_numbers(number), do: number
 
   def build_alternative_1(file_name) do
     file = File.read(file_name)
@@ -32,7 +43,23 @@ defmodule FizzBuzzElixir do
   end
 
   def handle_file_read_alternative_2({:ok, result}) do
+    result
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def handle_file_read_alternative_3({:ok, result}) do
     list = String.split(result, ",")
     Enum.map(list, &String.to_integer/1)
+  end
+
+  def convert_and_evaluate_numbers_alternative_1(elem) do
+    number = String.to_integer(elem)
+    if rem(number,3) === 0 do
+      :fizz
+    else if rem(number, 5) === 0 do
+      :buzz
+    end
+    end
   end
 end
