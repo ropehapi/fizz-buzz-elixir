4d01b2281190a465b288715388248e45e2cfe840
ropehapi <ropehapi@gmail.com>
2024-07-24 01:16:30 -0300

    Testando a aplicação

--- test/fizz_buzz_elixir_test.exs
+++ test/fizz_buzz_elixir_test.exs
@@ -1,8 +1,15 @@
 defmodule FizzBuzzElixirTest do
   use ExUnit.Case
-  doctest FizzBuzzElixir
 
-  test "greets the world" do
-    assert FizzBuzzElixir.hello() == :world
+  describe "build/1" do
+    test "when a valid file is provided, returns the converted list" do
+      expectedResponse = {:ok, [1, 2, :fizz, 4, :buzz, :buzz, :fizzbuzz, :buzz]}
+      assert FizzBuzzElixir.build("numbers.txt") == expectedResponse
+    end
+
+    test "when a invalid file is provided, returns an error" do
+      expectedResponse = {:error, "Error reading the file: enoent"}
+      assert FizzBuzzElixir.build("wrong.txt") == expectedResponse
+    end
   end
 end
